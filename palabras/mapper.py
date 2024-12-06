8e2776e8ab07681eff88d68980935ea388baec70
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-12-06 00:38:19 -0400

    Update mapper.py to filter words by minimum length; add longitud_minima variable for configurable filtering

--- palabras/mapper.py
+++ palabras/mapper.py
@@ -1,8 +1,11 @@
 import sys
 import re
 
+longitud_minima = 0
+
 for line in sys.stdin:
     palabras = line.strip().lower().split()
     for palabra in palabras:
-        if palabra.isalpha():  # Filtra palabras que solo contienen letras
+        if palabra.isalpha() and len(palabra) >= longitud_minima:
+        #if palabra.isalpha():  # Filtra palabras que solo contienen letras
             print(f"{palabra}\t1")
